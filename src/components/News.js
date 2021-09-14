99f65592f928a59233fca30758b4884627de557a
gaurav147-star <gauravgupta9158@gmail.com>
2021-09-14 16:10:36 +0530

    next disabled

--- src/components/News.js
+++ src/components/News.js
@@ -2,62 +2,95 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export default class News extends Component {
-  
-  articles=[
-      {
-        "source": { "id": "goteborgs-posten", "name": "Göteborgs-Posten" },
-        "author": "Eric Hilmersson",
-        "title": "Hans syster dödades av talibanerna: \"Hon blev en måltavla”",
-        "description": "Cricketklubben i Göteborg blir ett ljus i det afghanska mörkret.",
-        "url": "http://www.gp.se/sport/g%C3%B6teborg-king-cricket-club-%C3%A4r-ett-ljus-i-m%C3%B6rkret-f%C3%B6r-afghanska-flyktingar-1.54493942",
-        "urlToImage": "http://www.gp.se/image/policy:1.54493903:1631502019/image.jpg?f=Wide%26w=1200%26%24p%24f%24w=834285a",
-        "publishedAt": "2021-09-13T05:00:00Z",
-        "content": null
-      },
-      {
-        "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-        "author": null,
-        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-        "publishedAt": "2020-04-27T11:41:47Z",
-        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-      },
-      {
-        "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-        "author": null,
-        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-        "publishedAt": "2020-03-30T15:26:05Z",
-        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-      }
-    ]
-  
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      articles:this.articles,
-      loading:false
-    }
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
 
-}
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=72682cfdc09742139946d4462b14c722&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  }
+
+  handlePrevClick = async () => {
+    console.log("prev");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=72682cfdc09742139946d4462b14c722&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+
+  handleNextClick = async () => {
+    console.log("next");
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=72682cfdc09742139946d4462b14c722&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
+
   render() {
     return (
       <div className="container my-5">
-        <h2>News-Spot</h2>
+        <h1 className="text-center">News-Spot</h1>
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="mytile" description="mydecp" imageUrl="http://www.gp.se/image/policy:1.54493903:1631502019/image.jpg?f=Wide%26w=1200%26%24p%24f%24w=834285a"/>
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="mytile" description="mydecp" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="mytile" description="mydecp" />
-          </div> 
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4 my-5" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 45) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 70) : ""
+                  }
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+          disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
-    constructor(){
-        super();
-
-    }
     render() {
-    let {title,description,imageUrl}=this.props;
+    let {title,description,imageUrl,newsUrl}=this.props;
         return (
             <div>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
+                    <img src={!imageUrl?"https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg":imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href="/newsdetails" className="btn btn-sm btn-primary">More</a>
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark btn-primary">More</a>
                     </div>
                 </div>
             </div>
